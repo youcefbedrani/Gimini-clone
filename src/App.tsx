ced17f7bd64d94c901b9ce33d4cc9e78d0cdfd86
youcefbedrani <youcefbedrani99@gmail.com>
2024-09-13 02:58:11 +0100

    initial gemini google clone

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-slate-400">
+      <h3 className="text-green-400 text-bold">Hi</h3>
+    </div>
+  );
+}
+
+export default App;
